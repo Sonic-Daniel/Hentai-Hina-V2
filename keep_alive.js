3793ea9883d9b10f1bf1c0b648f3683314627063
⫷㊚ç𝖎𝖉×͜×𝖐𝖆𝖌𝖊𝖓𝖔㊚⫸ <sorokomarco@gmail.com>
2024-09-29 07:56:24 +0000

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,8 @@
+// keep_alive.js
 var http = require('http');
 
+// Créer un serveur HTTP qui renvoie "I'm alive" pour chaque requête
 http.createServer(function (req, res) {
     res.write("I'm alive");
     res.end();
-}).listen(8080);
+}).listen(8080); // Écoute sur le port 8080
