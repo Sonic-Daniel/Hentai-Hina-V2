93a2711343c4cbd27c9370042eca8de0cbb0f7bf
ʚɸɞ Sønïč ʚɸɞ <danielthiam127@gmail.com>
2024-09-29 18:32:21 +0100

    Update imgur.js

--- scripts/cmds/imgur.js
+++ scripts/cmds/imgur.js
@@ -1,33 +1,34 @@
 const axios = require('axios');
 
-module.exports = {
-  config: {
-    name: "imgur",
-    version: "1.0",
-    author: "otinxsandip",
-    countDown: 5,
-    role: 0,
-    longDescription: "Imgur link",
-    category: "image",
-    guide: {
-      en: "${pn} reply to image"
-    }
-  },
+module.exports.config = {
+	name: "imgur",
+	version: "3.0",
+	role: 0,
+	credits: "kenglie",
+	description: "imgur upload",
+	hasPrefix: false,
+	usages: "[reply to image]",
+	cooldown: 5,
+	aliases: ["im"]
+};
 
-  onStart: async function ({ message, api, event }) {
-    const puti = event.messageReply?.attachments[0]?.url;
+module.exports.run = async ({ api, event }) => {
+	let link2;
 
-    if (!puti) {
-      return message.reply('Please reply to an image.');
-    }
+	if (event.type === "message_reply" && event.messageReply.attachments.length > 0) {
+		link2 = event.messageReply.attachments[0].url;
+	} else if (event.attachments.length > 0) {
+		link2 = event.attachments[0].url;
+	} else {
+		return api.sendMessage('No attachment detected. Please reply to an image.', event.threadID, event.messageID);
+	}
 
-    try {
-      const res = await axios.get(`https://sandipapi.onrender.com/imgur?link=${encodeURIComponent(puti)}`);
-      const lado = res.data.uploaded.image;
-      return message.reply(lado);
-    } catch (error) {
-      console.error(error);
-      return message.reply('api sucks bro.');
-    }
-  }
+	try {
+		const res = await axios.get(`https://eurix-api.replit.app/imgur?link=${encodeURIComponent(link2)}`);
+		const link = res.data.uploaded.image;
+		return api.sendMessage(`Here is the Imgur link for the image you provided:\n\n${link}`, event.threadID, event.messageID);
+	} catch (error) {
+		console.error("Error uploading image to Imgur:", error);
+		return api.sendMessage("An error occurred while uploading the image to Imgur.", event.threadID, event.messageID);
+	}
 };
